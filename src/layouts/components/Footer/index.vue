0b198e8b0e4f100c79e9e0d162852dcc6dbde643
杭麟 <hanglin@kalendsoft.com>
2023-07-06 09:00:18 +0800

    revert: 修改生产api地址，添加备案号

--- src/layouts/components/Footer/index.vue
+++ src/layouts/components/Footer/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="footer flx-center">
-    <a href="https://github.com/HalseySpicy" target="_blank"> 2022 © 后台管理系统 By hl Technology. </a>
+    <a href="https://github.com/yiranHang/admin.hl.git" target="_blank"> 后台管理系统 By hl Technology. </a>
+    <a href="https://beian.miit.gov.cn/" target="_blank">&nbsp;备案 | </a>
+    <a href="https://beian.miit.gov.cn/" target="_blank">&nbsp;沪ICP备2023017183号-1</a>
   </div>
 </template>
 
