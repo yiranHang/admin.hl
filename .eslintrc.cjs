5092e4767fa60ea73181f4ffa4385117e74709b5
杭麟 <hanglin@kalendsoft.com>
2023-07-12 14:06:38 +0800

    chore: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,31 @@
+// @see: http://eslint.cn
+
 module.exports = {
   root: true,
   env: {
     browser: true,
-    es2021: true,
     node: true,
-    'vue/setup-compiler-macros': true,
+    es6: true
   },
+  // 指定如何解析语法
   parser: 'vue-eslint-parser',
+  // 优先级低于 parse 的语法解析配置
   parserOptions: {
     parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
     sourceType: 'module',
     jsxPragma: 'React',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  plugins: ['vue', '@typescript-eslint'],
+  // 继承某些已有的规则
+  extends: ['plugin:vue/vue3-recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  /**
+   * "off" 或 0    ==>  关闭规则
+   * "warn" 或 1   ==>  打开的规则作为警告（不影响代码执行）
+   * "error" 或 2  ==>  规则作为一个错误（代码不能执行，界面报错）
+   */
   rules: {
     // eslint (http://eslint.cn/docs/rules)
     'no-var': 'error', // 要求使用 let 或 const 而不是 var
@@ -52,10 +55,6 @@ module.exports = {
     'vue/attributes-order': 'off', // vue api使用顺序，强制执行属性顺序
     'vue/no-v-html': 'off', // 禁止使用 v-html
     'vue/require-default-prop': 'off', // 此规则要求为每个 prop 为必填时，必须提供默认值
-    'vue/multi-word-component-names': 'off', // 要求组件名称始终为 “-” 链接的单词
-  },
-  globals: {
-    window: true,
-    NodeJS: true,
-  },
+    'vue/multi-word-component-names': 'off' // 要求组件名称始终为 “-” 链接的单词
+  }
 }
