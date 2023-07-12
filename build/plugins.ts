7355314ca14d647e03f226a8b760eb09a5ffd7c5
杭麟 <hanglin@kalendsoft.com>
2023-07-12 14:04:07 +0800

    style: 格式化代码

--- build/plugins.ts
+++ build/plugins.ts
@@ -1,21 +1,21 @@
-import { resolve } from "path";
-import { PluginOption } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import { visualizer } from "rollup-plugin-visualizer";
-import { createHtmlPlugin } from "vite-plugin-html";
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import eslintPlugin from "vite-plugin-eslint";
-import viteCompression from "vite-plugin-compression";
-import vueSetupExtend from "unplugin-vue-setup-extend-plus/vite";
+import { resolve } from 'path'
+import { PluginOption } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import eslintPlugin from 'vite-plugin-eslint'
+import viteCompression from 'vite-plugin-compression'
+import vueSetupExtend from 'unplugin-vue-setup-extend-plus/vite'
 
 /**
  * 创建 vite 插件
  * @param viteEnv
  */
 export const createVitePlugins = (viteEnv: ViteEnv): (PluginOption | PluginOption[])[] => {
-  const { VITE_GLOB_APP_TITLE, VITE_REPORT, VITE_PWA } = viteEnv;
+  const { VITE_GLOB_APP_TITLE, VITE_REPORT, VITE_PWA } = viteEnv
   return [
     vue(),
     // vue 可以使用 jsx/tsx 语法
@@ -34,75 +34,75 @@ export const createVitePlugins = (viteEnv: ViteEnv): (PluginOption | PluginOptio
     }),
     // 使用 svg 图标
     createSvgIconsPlugin({
-      iconDirs: [resolve(process.cwd(), "src/assets/icons")],
-      symbolId: "icon-[dir]-[name]"
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+      symbolId: 'icon-[dir]-[name]'
     }),
     // vitePWA
     VITE_PWA && createVitePwa(viteEnv),
     // 是否生成包预览，分析依赖包大小做优化处理
-    VITE_REPORT && (visualizer({ filename: "stats.html", gzipSize: true, brotliSize: true }) as PluginOption)
-  ];
-};
+    VITE_REPORT && (visualizer({ filename: 'stats.html', gzipSize: true, brotliSize: true }) as PluginOption)
+  ]
+}
 
 /**
  * @description 根据 compress 配置，生成不同的压缩规则
  * @param viteEnv
  */
 const createCompression = (viteEnv: ViteEnv): PluginOption | PluginOption[] => {
-  const { VITE_BUILD_COMPRESS = "none", VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE } = viteEnv;
-  const compressList = VITE_BUILD_COMPRESS.split(",");
-  const plugins: PluginOption[] = [];
-  if (compressList.includes("gzip")) {
+  const { VITE_BUILD_COMPRESS = 'none', VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE } = viteEnv
+  const compressList = VITE_BUILD_COMPRESS.split(',')
+  const plugins: PluginOption[] = []
+  if (compressList.includes('gzip')) {
     plugins.push(
       viteCompression({
-        ext: ".gz",
-        algorithm: "gzip",
+        ext: '.gz',
+        algorithm: 'gzip',
         deleteOriginFile: VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE
       })
-    );
+    )
   }
-  if (compressList.includes("brotli")) {
+  if (compressList.includes('brotli')) {
     plugins.push(
       viteCompression({
-        ext: ".br",
-        algorithm: "brotliCompress",
+        ext: '.br',
+        algorithm: 'brotliCompress',
         deleteOriginFile: VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE
       })
-    );
+    )
   }
-  return plugins;
-};
+  return plugins
+}
 
 /**
  * @description VitePwa
  * @param viteEnv
  */
 const createVitePwa = (viteEnv: ViteEnv): PluginOption | PluginOption[] => {
-  const { VITE_GLOB_APP_TITLE } = viteEnv;
+  const { VITE_GLOB_APP_TITLE } = viteEnv
   return VitePWA({
-    registerType: "autoUpdate",
+    registerType: 'autoUpdate',
     manifest: {
       name: VITE_GLOB_APP_TITLE,
       short_name: VITE_GLOB_APP_TITLE,
-      theme_color: "#ffffff",
+      theme_color: '#ffffff',
       icons: [
         {
-          src: "/logo.png",
-          sizes: "192x192",
-          type: "image/png"
+          src: '/logo.png',
+          sizes: '192x192',
+          type: 'image/png'
         },
         {
-          src: "/logo.png",
-          sizes: "512x512",
-          type: "image/png"
+          src: '/logo.png',
+          sizes: '512x512',
+          type: 'image/png'
         },
         {
-          src: "/logo.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any maskable"
+          src: '/logo.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
         }
       ]
     }
-  });
-};
+  })
+}
