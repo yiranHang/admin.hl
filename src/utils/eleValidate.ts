9e817db2a9e3916d15f3eeeef6d17f91a61d785f
杭麟 <hanglin@kalendsoft.com>
2023-07-20 16:34:22 +0800

    chore: 删除未使用的参数

--- src/utils/eleValidate.ts
+++ src/utils/eleValidate.ts
@@ -4,7 +4,7 @@ class Validate {
   /**
    *  @rule 手机号
    */
-  checkPhoneNumber(rule: any, value: any, callback: any) {
+  checkPhoneNumber(value: any, callback: any) {
     const regexp =
       /^(((13[0-9]{1})|(15[0-9]{1})|(16[0-9]{1})|(17[3-8]{1})|(18[0-9]{1})|(19[0-9]{1})|(14[5-7]{1}))+\d{8})$/
     if (value === '') callback('请输入手机号码')
@@ -21,7 +21,7 @@ class Validate {
    * @param val
    * @returns
    */
-  checkPassword(rule: any, value: any, callback: any) {
+  checkPassword(value: any, callback: any) {
     const regexp = /^[a-zA-Z0-9_-]{4,16}$/
     const msg = '长度大于4且小于16;包含大小写字母、数字、“-”、“_”'
     if (value && !regexp.test(value)) {
@@ -37,7 +37,7 @@ class Validate {
    * @param val
    * @returns
    */
-  checkCode(rule: any, value: any, callback: any) {
+  checkCode(value: any, callback: any) {
     const regexp = /^[a-zA-Z0-9_-]{2,20}$/
     const msg = '长度2到20，大小写字母、数字、“_”、“-”'
     if (value && !regexp.test(value)) {
@@ -53,7 +53,7 @@ class Validate {
    * @param val
    * @returns
    */
-  checkName(rule: any, value: any, callback: any) {
+  checkName(value: any, callback: any) {
     const regexp = /^[A-z0-9\u4e00-\u9fa5]{1,10}$/
     const msg = '长度1到10，只能中文、数字和字母'
     if (value && !regexp.test(value)) {
@@ -69,7 +69,7 @@ class Validate {
    * @param val
    * @returns
    */
-  checkPath(rule: any, value: any, callback: any) {
+  checkPath(value: any, callback: any) {
     const regexp = /^[/][a-zA-Z/]{1,}$/
     const msg = '必须以/开头，且只能包含大小写字母'
     if (value && !regexp.test(value)) {
